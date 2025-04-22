c90d190af87aaba0d3df441464d3523c603d9744
Júlia <julia.beatriz206487@gmail.com>
2025-04-22 18:05:21 -0300

    revisao04 feita

---
+++ revisao04.py
@@ -0,0 +1,6 @@
+#Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor
+
+num = int(input(f"Digite um numero para saber seu sucessor e antecessor: "))
+antecessor = num -1
+sucessor = num + 1
+print (antecessor, num, sucessor)
