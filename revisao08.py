06aabe3cf978fc9e8d8fad3164c9db8575037885
Júlia <julia.beatriz206487@gmail.com>
2025-04-22 19:36:54 -0300

    revisao09 feita

--- revisao08.py
+++ revisao08.py
@@ -1,4 +1,4 @@
-#USANDO FOR, MOSTRE OS NÚMEROS ÍMPARES DE 1 A 500
-for i in range(1, 500, 1):
+#USANDO FOR, MOSTRE OS NÚMEROS ÍMPARES DE 1 A 500 e depois com while
+for i in range(1, 501):
     if i % 2 != 0:
         print(i, end = " ")
